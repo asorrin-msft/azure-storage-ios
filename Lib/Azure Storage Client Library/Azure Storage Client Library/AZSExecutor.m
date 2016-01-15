d4747b37f2b15fcd361071ec65a76a3493ab569a
asorrin-msft <asorrin@microsoft.com>
2016-01-14 16:23:11 -0800

    Fixing race condition in streaming.

--- Lib/Azure Storage Client Library/Azure Storage Client Library/AZSExecutor.m
+++ Lib/Azure Storage Client Library/Azure Storage Client Library/AZSExecutor.m
@@ -448,6 +448,7 @@
     @autoreleasepool {
         self.runLoopForDownload = [NSRunLoop currentRunLoop];
         [outputStream scheduleInRunLoop:self.runLoopForDownload forMode:NSDefaultRunLoopMode];
+        [self.outputStream open];
         dispatch_semaphore_signal(self.semaphoreForRunloopCreation);
         
         // TODO: Make the below timeout value for the runloop configurable.
@@ -516,6 +517,7 @@
     self.runLoopForDownload = self.requestOptions.runLoopForDownload;
     if (self.runLoopForDownload == nil)
     {
+        // In this case, we will open the stream inside the createAndSpinRunloopWithOutputStream method.
         self.semaphoreForRunloopCreation = dispatch_semaphore_create(0);
 
         [NSThread detachNewThreadSelector:@selector(createAndSpinRunloopWithOutputStream:) toTarget:self withObject:self.outputStream];
@@ -525,10 +527,10 @@
     else
     {
         [self.outputStream scheduleInRunLoop:self.runLoopForDownload forMode:NSDefaultRunLoopMode];
+        [self.outputStream open];
     }
     
     
-    [self.outputStream open];
 
     completionHandler(NSURLSessionResponseAllow);
 }
